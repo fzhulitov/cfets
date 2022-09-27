66abd5679e4ad25e3d2a65de1645eac0b91bd7b4
Sergey Kikevich <sergey@rostsber.ru>
2022-09-27 17:45:40 +0300

    chore: add black to dev dependencies

--- cfets/request_data.py
+++ cfets/request_data.py
@@ -5,10 +5,11 @@ import requests
 URL = "https://www.chinamoney.com.cn/ags/ms/cm-u-bk-currency/"
 
 
-def get_data_frame(code: str = 'LprChrtCSV', start_date: str = '2013-10-01', end_date: str = None) -> dict:
+def get_data_frame(
+    code: str = "LprChrtCSV", start_date: str = "2013-10-01", end_date: str = None
+) -> dict:
     request_url = URL + code
-    params = {'startDate': start_date,
-              'endDate': end_date}
+    params = {"startDate": start_date, "endDate": end_date}
     r = requests.get(request_url, params=params)
     if r.status_code != requests.codes.ok:
         raise Exception(r.status_code, r.reason, request_url)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import cfets.lpr
 
-s = cfets.get_lpr_5y(start_date='2022-01-01')
+s = cfets.get_lpr_5y(start_date="2022-01-01")
 print(s)
