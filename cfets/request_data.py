efb76cb7f0f6043c41016b163fb5ead82dbb381b
fzhulitov <fzhulitov@gmail.com>
2022-11-18 14:29:42 +0200

    Finaly

--- cfets/request_data.py
+++ cfets/request_data.py
@@ -9,12 +9,9 @@ def get_data_frame(code: str = 'LprChrtCSV', start_date: str = '2013-10-01', end
     request_url = URL + code
     user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                                 'Chrome/89.0.4389.72 Safari/537.36'}
-    params = {'startDate': start_date,
-              'endDate': end_date}
-    abc = requests.get(request_url, headers=user_agent, params=params)
-    jresp = json.loads(abc.text)
+
     params = {"startDate": start_date, "endDate": end_date}
-    r = requests.get(request_url, params=params)
+    r = requests.get(request_url, headers=user_agent, params=params)
     if r.status_code != requests.codes.ok:
         raise Exception(r.status_code, r.reason, request_url)
     jresp = json.loads(r.text)
