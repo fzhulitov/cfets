d57f5663b01ce29cff86691fdc9e9e9fff77d187
fzhulitov <fzhulitov@gmail.com>
2023-01-10 13:59:28 +0200

    Swithing to 3.11 and params as pd.Timestamp

--- cfets/lpr.py
+++ cfets/lpr.py
@@ -4,19 +4,23 @@ import pandas as pd
 from cfets import request_data
 
 lpr_code = "LprChrtCSV"
+sd = pd.Timestamp(2013, 10, 1)
 
-
-def get_lpr_1y(start_date: str = "2013-10-01", end_date: str = None) -> pd.Series:
-    d = request_data.get_data_frame(
-        code=lpr_code, start_date=start_date, end_date=end_date
+def get_lpr_1y(start_date: pd.Timestamp = sd,
+               end_date: pd.Timestamp = None) -> pd.Series:
+    d = request_data.get_data_frame(code=lpr_code,
+                start_date=start_date.strftime("%Y-%m-%d"),
+                end_date=end_date
     )
     df = prepare_data(d)
     return df.loc[:, "lpr1y"].dropna()
 
 
-def get_lpr_5y(start_date: str = "2013-10-01", end_date: str = None) -> pd.Series:
+def get_lpr_5y(start_date: pd.Timestamp = sd,
+               end_date: pd.Timestamp = None) -> pd.Series:
     d = request_data.get_data_frame(
-        code=lpr_code, start_date=start_date, end_date=end_date
+        code=lpr_code, start_date=start_date.strftime("%Y-%m-%d"),
+        end_date=end_date
     )
     df = prepare_data(d)
     return df.loc[:, "lpr5y"].dropna()
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import pandas as pd
 import cfets.lpr
 
-s = cfets.get_lpr_5y(start_date="2022-01-01")
+sd = pd.Timestamp(2022, 1, 1)
+print(sd)
+
+s = cfets.get_lpr_5y(start_date=sd)
 print(s)
-s = cfets.get_lpr_1y(start_date="2022-01-01")
+s = cfets.get_lpr_1y(start_date=sd)
 print(s)
